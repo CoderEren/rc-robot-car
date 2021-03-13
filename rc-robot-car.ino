d8bb56748e8f6f945fe5f38cacc983d2db0c1f8d
Eren Geridonmez <65499630+CoderEren@users.noreply.github.com>
2021-03-13 21:34:05 +0000

    Update rc-robot-car.ino

--- rc-robot-car.ino
+++ rc-robot-car.ino
@@ -1,3 +1,6 @@
+const int pingPin = 8; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+
 // motor one
 int enA = 9;
 int in1 = 2;
@@ -17,17 +20,47 @@ decode_results results;
 void setup() {
   irrecv.enableIRIn();
   irrecv.blink13(true);
+  pinMode(pingPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  analogWrite(enA, 200);
-  analogWrite(enB, 200);
+  analogWrite(enA, 150);
+  analogWrite(enB, 150);
 }
 
 void loop() {
+
+  long duration, cm;
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  cm = (duration * 0.034) / 2;
+
+  if (cm >= 10) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);  
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    delay(1000);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    delay(1000);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);  
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
+
+  
   if (irrecv.decode(&results)){
         switch(results.value){
           case 0xFF38C7: //Keypad button "5"
@@ -39,10 +72,17 @@ void loop() {
 
         switch(results.value){
           case 0xFF18E7: //Keypad button "2"
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, HIGH);
-          digitalWrite(in3, LOW);
-          digitalWrite(in4, HIGH);
+          if (cm >= 10) {
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);  
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+          } else {
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+          }
         }
 
         switch(results.value){
@@ -55,18 +95,32 @@ void loop() {
 
         switch(results.value){
           case 0xFF10EF: //Keypad button "4"
+          if (cm >= 10) {
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);  
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+          } else {
           digitalWrite(in1, LOW);
           digitalWrite(in2, HIGH);
           digitalWrite(in3, LOW);
           digitalWrite(in4, LOW);
+          }
         }
 
         switch(results.value){
           case 0xFF5AA5: //Keypad button "6"
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, LOW);
-          digitalWrite(in3, LOW);
-          digitalWrite(in4, HIGH);
+          if (cm >= 10) {
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);  
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+          } else {
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+          }
         }
         irrecv.resume(); 
     }
